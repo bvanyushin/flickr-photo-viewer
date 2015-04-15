dc64992d2d36a2fe6dda7aa1a8e7ab5b5ba994a6
Boris Vanyushin <b.vanyushin@gmail.com>
2015-04-15 16:28:31 +0300

    paging added: change number of photos, go to next/prev/first/last pages

--- js/view_model.js
+++ js/view_model.js
@@ -14,16 +14,16 @@ function FlickrAppViewModel() {
     this.title = data.title;
   }
 
-  self.albums = ko.observableArray([]);
-  self.chosenAlbumID = ko.observable();
-  self.chosenAlbumPhotos = ko.observableArray([]);
-  self.chosenPhoto = ko.observable();
-
   var apiUrl = "https://api.flickr.com/services/rest/?";
   var user = "43266322@N06";
   var apiKey = "82aa4c342525ac3bf02945d35a2e1c7b";
   
-  self.getAlbums = function() {
+  // albums
+  self.albums = ko.observableArray([]);
+  self.currentAlbum = ko.observable();
+  self.currentAlbumContent = ko.observableArray([]);
+
+  self.getAlbumsList = function() {
     var method = "flickr.photosets.getList";
     var callUrl = apiUrl + "method=" + method + "&api_key=" + apiKey + "&user_id=" + user + "&format=json&nojsoncallback=1";
 
@@ -35,38 +35,92 @@ function FlickrAppViewModel() {
   }
 
   self.goToAlbum = function (album) {
-    self.chosenAlbumID(album);
-    self.chosenPhoto(null);
-    self.getAlbumPhotos();
+    self.currentAlbum(album);
+    self.getAlbumContent();
+    self.goToPage(1);
   }
 
-  self.getAlbumPhotos = function () {
+  self.getAlbumContent = function () {
     var method = "flickr.photosets.getPhotos";
-    var callUrl = apiUrl + "method=" + method + "&api_key=" + apiKey + "&photoset_id=" + self.chosenAlbumID().id + "&user_id=" + user + "&format=json&nojsoncallback=1";
+    var callUrl = apiUrl + "method=" + method + "&api_key=" + apiKey + "&photoset_id=" + self.currentAlbum().id + "&user_id=" + user + "&format=json&nojsoncallback=1";
     $.getJSON(callUrl, function(allData) {
       var mappedPhotos = $.map(allData.photoset.photo, function(item) { return new Photo(item) });
-      self.chosenAlbumPhotos(mappedPhotos);
-    }); 
+      self.currentAlbumContent(mappedPhotos);
+      self.goToPage(1);  // I don't like this place
+    });
   }
+
+  // photos
+
+  self.currentPhoto = ko.observable();
 
   self.loadPreview = function(element, index, data) {
     $("#" + index.id).attr('src', index.previewUrl);
   };
 
   self.loadPhoto = function(photo) {
-    self.chosenAlbumPhotos(null);
-     self.chosenPhoto(photo);
+    self.currentPageContent(null);
+    self.currentPhoto(photo);
   };
 
   self.sortPhotosByTitle = function() {
-    this.items = self.chosenAlbumPhotos();
+    this.items = self.currentAlbumContent();
     this.items.sort(function(a, b) {
       return a.title < b.title ? -1 : 1;
     });
-    self.chosenAlbumPhotos(this.items);
+    self.currentAlbumContent(this.items);
   }
 
-  self.getAlbums();
+  // pages
+
+  self.pageCount = ko.pureComputed(function() {
+    return Math.ceil( self.currentAlbumContent().length / self.photosPerPage() );
+  });
+  self.currentPage = ko.observable();
+  self.currentPageContent = ko.observableArray([]);
+  self.photosPerPage = ko.observable(9);
+  self.pageSizesCollection = [3, 6, 9, 12];
+
+  self.photosPerPage.subscribe(function() {
+    self.goToFirstPage();
+  });
+
+  self.goToPage = function(num) {
+    self.currentPage(num);
+    self.currentPhoto(null);
+    self.getCurrentPageContent();
+  }
+
+  self.goToFirstPage = function() {
+    self.goToPage(1);
+  }
+
+  self.goToPreviousPage = function() {
+    if (self.currentPage() > 1) 
+      self.goToPage(self.currentPage() - 1)
+    else
+      self.goToFirstPage()
+  }
+
+  self.goToLastPage = function() {
+    self.goToPage(self.pageCount());
+  }
+
+  self.goToNextPage = function() {
+    if (self.currentPage() < self.pageCount()) 
+      self.goToPage(self.currentPage() + 1)
+    else
+      self.goToLastPage()
+  }
+
+  self.getCurrentPageContent = function() {
+    var fromIndex = (self.currentPage() - 1) * self.photosPerPage();
+    var toIndex = fromIndex + self.photosPerPage();
+    self.currentPageContent(self.currentAlbumContent().slice(fromIndex, toIndex));
+  }
+
+  // init
+  self.getAlbumsList();
 };
 
 
