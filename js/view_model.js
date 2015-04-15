3f22fee08c901f407881311777b60ddec830c48f
Boris Vanyushin <b.vanyushin@gmail.com>
2015-04-15 10:12:15 +0300

    base functional implemented

--- js/view_model.js
+++ js/view_model.js
@@ -1,30 +1,33 @@
-function ViewModel() {
+function FlickrAppViewModel() {
   var self = this;
 
   function Album(data) {
     this.id = data.id;
     this.title = data.title._content;
+    this.description = data.description._content;
   }
 
   function Photo (data) {
     this.id = data.id;
     this.url = 'http://farm' + data.farm + '.static.flickr.com/' + data.server + '/' + data.id + '_' + data.secret + '.jpg';
     this.previewUrl = 'http://farm' + data.farm + '.static.flickr.com/' + data.server + '/' + data.id + '_' + data.secret + '_m.jpg';
+    this.title = data.title;
   }
 
   self.albums = ko.observableArray([]);
   self.chosenAlbumID = ko.observable();
-  self.chosenAlbumPhotos = ko.observable();
+  self.chosenAlbumPhotos = ko.observableArray([]);
   self.chosenPhoto = ko.observable();
 
+  var apiUrl = "https://api.flickr.com/services/rest/?";
+  var user = "43266322@N06";
+  var apiKey = "82aa4c342525ac3bf02945d35a2e1c7b";
+  
   self.getAlbums = function() {
-    var apiUrl = "https://api.flickr.com/services/rest/?";
-    var user = "43266322@N06";
     var method = "flickr.photosets.getList";
-    var apiKey = "82aa4c342525ac3bf02945d35a2e1c7b";
-    var apiUrl = apiUrl + "method=" + method + "&api_key=" + apiKey + "&user_id=" + user + "&format=json&nojsoncallback=1";
+    var callUrl = apiUrl + "method=" + method + "&api_key=" + apiKey + "&user_id=" + user + "&format=json&nojsoncallback=1";
 
-    $.getJSON(apiUrl, function(allData) {
+    $.getJSON(callUrl, function(allData) {
       var mappedAlbums = $.map(allData.photosets.photoset, function(item) { return new Album(item) });
       self.albums(mappedAlbums);
       self.goToAlbum(mappedAlbums[0]);
@@ -38,12 +41,9 @@ function ViewModel() {
   }
 
   self.getAlbumPhotos = function () {
-    var apiUrl = "https://api.flickr.com/services/rest/?";
-    var user = "43266322@N06";
     var method = "flickr.photosets.getPhotos";
-    var apiKey = "82aa4c342525ac3bf02945d35a2e1c7b";
-    var apiUrl = apiUrl + "method=" + method + "&api_key=" + apiKey + "&photoset_id=" + self.chosenAlbumID().id + "&user_id=" + user + "&format=json&nojsoncallback=1";
-    $.getJSON(apiUrl, function(allData) {
+    var callUrl = apiUrl + "method=" + method + "&api_key=" + apiKey + "&photoset_id=" + self.chosenAlbumID().id + "&user_id=" + user + "&format=json&nojsoncallback=1";
+    $.getJSON(callUrl, function(allData) {
       var mappedPhotos = $.map(allData.photoset.photo, function(item) { return new Photo(item) });
       self.chosenAlbumPhotos(mappedPhotos);
     }); 
@@ -53,13 +53,22 @@ function ViewModel() {
     $("#" + index.id).attr('src', index.previewUrl);
   };
 
-  self.loadPhoto = function(element, index, data) {
-    $("#" + index.id).attr('src', index.url);
+  self.loadPhoto = function(photo) {
+    self.chosenAlbumPhotos(null);
+     self.chosenPhoto(photo);
   };
+
+  self.sortPhotosByTitle = function() {
+    this.items = self.chosenAlbumPhotos();
+    this.items.sort(function(a, b) {
+      return a.title < b.title ? -1 : 1;
+    });
+    self.chosenAlbumPhotos(this.items);
+  }
 
   self.getAlbums();
 };
 
 
-viewModel = new ViewModel;
-ko.applyBindings(viewModel)
+flickrAppViewModel = new FlickrAppViewModel;
+ko.applyBindings(flickrAppViewModel);
