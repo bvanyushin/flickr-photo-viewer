9b9c57d19fddd51b3eb2e2d7805755dcc078b690
Boris Vanyushin <b.vanyushin@gmail.com>
2015-04-16 15:23:04 +0300

    Little DRY implemented

--- js/view_model.js
+++ js/view_model.js
@@ -17,6 +17,7 @@ function FlickrAppViewModel() {
   var apiUrl = "https://api.flickr.com/services/rest/?";
   var user = "43266322@N06";
   var apiKey = "82aa4c342525ac3bf02945d35a2e1c7b";
+  var callbackFormat = "&format=json&nojsoncallback=1"
   
   // albums
   self.albums = ko.observableArray([]);
@@ -25,7 +26,7 @@ function FlickrAppViewModel() {
 
   self.getAlbumsList = function() {
     var method = "flickr.photosets.getList";
-    var callUrl = apiUrl + "method=" + method + "&api_key=" + apiKey + "&user_id=" + user + "&format=json&nojsoncallback=1";
+    var callUrl = apiUrl + "method=" + method + "&api_key=" + apiKey + "&user_id=" + user + callbackFormat;
 
     $.getJSON(callUrl, function(allData) {
       var mappedAlbums = $.map(allData.photosets.photoset, function(item) { return new Album(item) });
@@ -42,7 +43,7 @@ function FlickrAppViewModel() {
 
   self.getAlbumContent = function () {
     var method = "flickr.photosets.getPhotos";
-    var callUrl = apiUrl + "method=" + method + "&api_key=" + apiKey + "&photoset_id=" + self.currentAlbum().id + "&user_id=" + user + "&format=json&nojsoncallback=1";
+    var callUrl = apiUrl + "method=" + method + "&api_key=" + apiKey + "&photoset_id=" + self.currentAlbum().id + "&user_id=" + user + callbackFormat;
     $.getJSON(callUrl, function(allData) {
       var mappedPhotos = $.map(allData.photoset.photo, function(item) { return new Photo(item) });
       self.currentAlbumContent(mappedPhotos);
